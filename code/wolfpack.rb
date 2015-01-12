82ef71f2e7749330ec2900d6c4efdbbc793439d0
palmbeachjeff <palmbeachjeff@gmail.com>
2015-01-12 15:23:07 -0500

    additional terms added to intial set up

--- code/wolfpack.rb
+++ code/wolfpack.rb
@@ -22,12 +22,15 @@ if wolf.join.pack && wolf.becomes_leader
 #A wolf can join a pack. 
 #If a wolf joins a pack and becomes leader, the pack throws out the former leader.
 
+number_wolves_eat_gazelle >= 2
+number_wolves_eat_water buffalo >=5
+number_wolves_eat_rhinoceros >=30
 
 
-A wolfpack can take down different sized things depending on how big it is.
+#A wolfpack can take down different sized things depending on how big it is.
 
-A pack of 2 wolves can hunt a gazelle.
-A pack of 5 wolves can get a water buffalo.
-A pack of 30 wolves can eat a rhinoceros.
-I used a lot of synonyms there in the hunting section. 
-You don't need methods for all of them.
+#A pack of 2 wolves can hunt a gazelle.
+#A pack of 5 wolves can get a water buffalo.
+#A pack of 30 wolves can eat a rhinoceros.
+#I used a lot of synonyms there in the hunting section. 
+#You don't need methods for all of them.
