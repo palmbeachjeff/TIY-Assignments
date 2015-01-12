3b74373c79f966340baf07589096cc78acbe4e46
palmbeachjeff <palmbeachjeff@gmail.com>
2015-01-12 15:16:28 -0500

    Wolfpack and wolf are in initial form

---
+++ code/wolf.rb
@@ -0,0 +1,18 @@
+
+
+	def initialize wolf = (name, age)      #A wolf has a name and an age. 
+
+	end
+
+	def wolf_howl == true
+	#A wolf can howl. 
+	end
+
+	wolf_howl.louder 
+	if wolf_age > 3
+	else
+		wolf_howl.normal
+	#It howls louder if it's older than 3 than if it's younger. 
+
+	does_howl = call_name_of_wolf  	
+#If you call out the name of a wolf to its pack, that wolf will howl.
---
+++ code/wolfpack.rb
@@ -0,0 +1,33 @@
+
+def wolfpack ==     #A wolfpack is a group of wolves
+	@wolves 
+end
+
+def leader_wolfpack == oldest_wolf  #The leader of the wolfpack is the oldest wolf. 
+
+
+def submissive 
+end
+
+def dominant
+end
+
+
+#All wolves are submissive to older wolves and dominant over younger wolves.
+
+wolf.join.pack
+if wolf.join.pack && wolf.becomes_leader
+	pack.throws_out_former_leader
+
+#A wolf can join a pack. 
+#If a wolf joins a pack and becomes leader, the pack throws out the former leader.
+
+
+
+A wolfpack can take down different sized things depending on how big it is.
+
+A pack of 2 wolves can hunt a gazelle.
+A pack of 5 wolves can get a water buffalo.
+A pack of 30 wolves can eat a rhinoceros.
+I used a lot of synonyms there in the hunting section. 
+You don't need methods for all of them.
