5da8a8dce5269233f9a857a4add9288f8a5a24d3
palmbeachjeff <palmbeachjeff@gmail.com>
2015-01-16 16:19:25 -0500

    first test, first friend the error message

--- code/poker_deck.rb
+++ code/poker_deck.rb
@@ -1,7 +1,12 @@
-class Deck
+class Card
 
 	def initialize (value, suit)
 		@value = value
 		@suit = suit 
-	
+		suit =%w[Diamonds Spades Clubs Hearts]
+		@cards = []
+		["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"].each do |cards| 
+	end
+	end
+end
 #Update
--- code/poker_deck_test.rb
+++ code/poker_deck_test.rb
@@ -1,8 +1,17 @@
-class DeckTest < MiniTest::Unit::TestCase
+require 'minitest/autorun'
+require './poker_deck.rb'
 
-	def setup
-    @deck = Deck.new
+class TestCard < MiniTest::Unit::TestCase
+
+	def setup 
+    @deck = Card.new
 	end
+
+	def test_deck_has_52_cards
+		assert_equal 52, @deck.size
+	end
+
 end
 
 #Update
+
